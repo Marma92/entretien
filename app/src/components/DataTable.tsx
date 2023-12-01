646c427da6d7e91f555fece3f25772cf38ce0cd6
Amram ELBAZ <elbaz.amram@gmail.com>
2023-12-01 03:06:39 +0100

    list of products

--- app/src/components/DataTable.tsx
+++ app/src/components/DataTable.tsx
@@ -1,40 +1,27 @@
 import * as React from 'react';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { useEffect, useState } from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Row } from '../interfaces/Row';
 
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
+  { field: 'title', headerName: 'Name', width: 500 },
+  { field: 'phase', headerName: 'Status', width: 150 },
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+const apiEndpoint = 'http://localhost:8080/api/mission-apprentissage/products-lazy'
 
 export default function DataTable() {
+  const [rows, setRows] = useState<Row[]>([]);
+  useEffect(() => {
+    fetch(apiEndpoint)
+      .then((response) => response.json())
+      .then((data : Row[] ) => {
+        const rowsWithId = data.map((row , index) => ({ id: index + 1, ...row }));
+        setRows(rowsWithId);
+      })
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <div style={{ height: 400, width: '100%' }}>
       <DataGrid
@@ -45,8 +32,8 @@ export default function DataTable() {
             paginationModel: { page: 0, pageSize: 5 },
           },
         }}
+        pagination
         pageSizeOptions={[5, 10]}
-        checkboxSelection
       />
     </div>
   );
---
+++ app/src/interfaces/Row.ts
@@ -0,0 +1,5 @@
+export interface Row {
+  name: string;
+  phase: string;
+  path: string;
+}
